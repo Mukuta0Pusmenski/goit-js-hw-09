72eb8269f1eebcc4401ed3c173508a71fe7fae91
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2024-11-17 20:51:22 +0200

    Update index.html with correct paths

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
+import { sync as globSync } from 'glob';
+
+const inputFiles = globSync('./src/public/*.html');
+console.log('Input files:', inputFiles);
 
 export default defineConfig(({ command }) => {
   return {
@@ -13,19 +16,14 @@ export default defineConfig(({ command }) => {
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: inputFiles.length > 0 ? inputFiles : './src/public/index.html',
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
+          entryFileNames: '[name].js',
           assetFileNames: assetInfo => {
             if (assetInfo.name && assetInfo.name.endsWith('.html')) {
               return '[name].[ext]';
@@ -39,7 +37,7 @@ export default defineConfig(({ command }) => {
     },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/**.html']),
+      FullReload(['./src/public/**/**.html']),
       SortCss({
         sort: 'mobile-first',
       }),
